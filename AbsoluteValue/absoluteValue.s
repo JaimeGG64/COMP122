d1f95e74101a742b27c075bf7ee4602d10b74397
JaimeGG64 <jgarcia16432@gmail.com>
2019-02-20 21:03:27 -0800

    got absolute value program to change negative numbers to positive

--- AbsoluteValue/absoluteValue.s
+++ AbsoluteValue/absoluteValue.s
@@ -5,11 +5,20 @@ mov r1,#0 @ mode is input
 
 swi 0x66 @open file
 swi 0x6c @SWI_RdInt
+
 mov r1,r0
 mov r0,#1
-CMP   r0, #0   ; if (x <= 0)
-MOVLE r0, #3   ;   x = 0;
-MOVGT r0, #5
+
+sub r0,r2,r1
+
+mov r1,r0
+mov r0,#1
+
+@ mvn r1, r0
+
+@ cmp   r1, #0
+@ movle r1, 
+
 swi 0x6b @SWI_PrInt
 swi 0x68 @close file
 swi 0x11 @exit
