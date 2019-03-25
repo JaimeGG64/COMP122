a5c137f5c0d86c8d3fdbd422662cd4b741691a1b
JaimeGG64 <jgarcia16432@gmail.com>
2019-03-24 20:23:04 -0700

    got a general idea of how looping works with ARMSim

--- loop.s
+++ loop.s
@@ -1,8 +1,14 @@
-		
-	MOV r0, #0
-LOOP
-	ADD r0, r0, #1 
-	CMP r0, #10
-	BNE LOOP
+.equ SWI_Exit, 0x11
+@ mov r1, #2
 
-END
+cmp r1, #2   @prepare to compare r1
+blt loop @check if r1 < 10
+b exit @if false then func2 is triggered
+
+loop:
+    adds r1,r1,#1
+    cmp r1, #10
+    blt loop @check if r1 < 10
+    b exit @if false then func2 is triggered
+exit:
+    swi SWI_Exit
