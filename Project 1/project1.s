467d30914837a08120e5df6986d61209f2302f3b
JaimeGG64 <jgarcia16432@gmail.com>
2019-03-26 22:48:46 -0700

    program now reads a line of integers

--- Project 1/project1.s
+++ Project 1/project1.s
@@ -1,21 +1,34 @@
 File: .asciz "integers.txt"
+InFileError: .asciz "Unable to open file"
+    .align
+InFileHandle: .word 0
 check_num_of_integers_Message: .asciz " integers"
 .equ SWI_Exit, 0x11
 .equ SWI_Print_String, 0x02
 .equ SWI_Open, 0x66
+.equ SWI_RdStr, 0x6a
 .equ SWI_PrInt, 0x6b
 .equ SWI_RdInt, 0x6c
+.equ SWI_Close, 0x68
 
 _start:
     ldr r0,=File @ set Name for input file
     mov r1,#0 @ mode is input
-
     swi SWI_Open @open file
-    swi SWI_RdInt @SWI_RdInt
-    mov r1,r0
-    mov r0,#1
-    cmp   r1, #10   @prepare to compare r1
-    b _exit
+    mov r4,r0
+    swi SWI_RdInt
+    mov r2,r0
+    mov r0,#0
+    cmp r1, #10 @prepare to compare r1
+    blt read_integers
+
+read_integers:
+    mov r0,r4
+    mov r1,#0
+    swi SWI_RdInt
+    mov r0,#0
+    cmp r3, #45
+    b read_integers
 
 check_num_of_integers:
 num_of_int_greater_than_x:
@@ -24,7 +37,6 @@ highest_int:
 
 _exit:
     swi SWI_PrInt @SWI_PrInt
-    ldr r0, =check_num_of_integers_Message
     swi SWI_Print_String
-    swi 0x68 @close file
+    swi SWI_Close @close file
     swi SWI_Exit @exit
