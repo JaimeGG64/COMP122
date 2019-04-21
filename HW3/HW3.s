741649d8c1fca8f20100a9b2739561e9ddc8bf74
JaimeGG64 <jgarcia16432@gmail.com>
2019-04-21 16:03:40 -0700

    program now reads and prints the sentance one character at a time

--- HW3/HW3.s
+++ HW3/HW3.s
@@ -14,12 +14,13 @@ CharArray: .skip 80
 
 OutFileHandle: .word 0
 
-.equ SWI_Exit, 0x11
+.equ SWI_Print_Char, 0x00
 .equ SWI_Print_String, 0x02
-.equ SWI_Open, 0x66
 .equ SWI_RdStr, 0x6a
 .equ SWI_PrInt, 0x6b
 .equ SWI_RdInt, 0x6c
+.equ SWI_Exit, 0x11
+.equ SWI_Open, 0x66
 .equ SWI_Close, 0x68
 .equ SWI_PrStr, 0x69
 
@@ -32,9 +33,34 @@ _start:
     mov r2,#80
     swi SWI_RdStr
 
+    mov r4, #80
+    cmp r4,#80
+    b read_sentance
+
     ldr r0,=OutFileName
     mov r1,#1
     swi SWI_Open
     mov r1,r4
 	swi SWI_PrStr
+
+read_sentance:
+    ldrb r0, [r1]
+
+    bl print
+
+    add r1,r1, #1
+    add r5,r5, #1
+
+    sub r4, r4, #1
+    
+    cmp r4, #0
+    bne read_sentance
+    b _exit
+
+print:
+    swi SWI_Print_Char
+	strb r0,[r1]
+	mov pc, r14
+
+_exit:
     swi SWI_Exit
