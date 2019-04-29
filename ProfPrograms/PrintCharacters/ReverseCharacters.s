4e7239cc761454899920dc1f358b9c9a50a112cb
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2019-04-28 22:50:04 -0700

    add professor reverseCharacters program

---
+++ ProfPrograms/PrintCharacters/ReverseCharacters.s
@@ -0,0 +1,25 @@
+.equ SWI_PrChr, 0x00
+.equ SWI_Exit, 0x11
+
+data:
+    .asciz "ABCDE"
+
+_start:
+    ldr r2,=data @point to string
+    add r2,r2,#4
+    mov r3,#5 @set the length of the String
+
+loop:
+    ldrb r0,[r2] @pick up first byte
+    swi SWI_PrChr @prints the single characters
+    mov r0, #'\n 
+    swi SWI_PrChr @prints next line
+    sub r2,r2,#1 @index to next
+    sub r3,r3,#1 @Subtracts the point
+    
+    cmp r3, #0 @prepare to compare
+    bne loop @If not equal return to loop
+    b _exit @Else exit
+
+_exit:
+    swi SWI_Exit @terminate the program
